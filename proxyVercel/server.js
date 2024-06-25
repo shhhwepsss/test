2607503e5395f3d8288030a728158fa52cc317ec
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-06-25 15:34:09 +0300

    gms

--- proxyVercel/server.js
+++ proxyVercel/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port =  3001;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
