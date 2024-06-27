d73525a7023f6dbd08256ba70959f17560295978
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-06-27 16:59:05 +0300

    Added env file for Mongo DB data

--- proxyVercel/app.js
+++ proxyVercel/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const Article = require('./models/article'); 
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
+
 //to-do: data to ENV
-const dbURI = 'mongodb+srv://timofeybabisashvili:0z6mAu9hofWdhe2v@cluster0.wzvxecj.mongodb.net/lolo-db?retryWrites=true&w=majority';
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
   .catch(err => console.error('Database connection error:', err));
